16b06983d034b47c7cc8f5ed00bbd79a3dde80eb
Jacob O'Bryant <git@jacobobryant.com>
2024-03-06 18:13:37 -0800

    Replace XTDB usages with Postgres

--- src/com/biffweb/example/postgres/worker.clj
+++ src/com/biffweb/example/postgres/worker.clj
@@ -1,31 +1,16 @@
 (ns com.biffweb.example.postgres.worker
   (:require [clojure.tools.logging :as log]
             [com.biffweb :as biff :refer [q]]
-            [xtdb.api :as xt]))
+            [next.jdbc :as jdbc]))
 
 (defn every-n-minutes [n]
   (iterate #(biff/add-seconds % (* 60 n)) (java.util.Date.)))
 
-(defn print-usage [{:keys [biff/db]}]
+(defn print-usage [{:keys [example/ds]}]
   ;; For a real app, you can have this run once per day and send you the output
   ;; in an email.
-  (let [n-users (nth (q db
-                        '{:find (count user)
-                          :where [[user :user/email]]})
-                     0
-                     0)]
+  (let [n-users (:count (jdbc/execute-one! ds ["SELECT count(*) FROM users"]))]
     (log/info "There are" n-users "users.")))
-
-(defn alert-new-user [{:keys [biff.xtdb/node]} tx]
-  (doseq [_ [nil]
-          :let [db-before (xt/db node {::xt/tx-id (dec (::xt/tx-id tx))})]
-          [op & args] (::xt/tx-ops tx)
-          :when (= op ::xt/put)
-          :let [[doc] args]
-          :when (and (contains? doc :user/email)
-                     (nil? (xt/entity db-before (:xt/id doc))))]
-    ;; You could send this as an email instead of printing.
-    (log/info "WOAH there's a new user")))
 
 (defn echo-consumer [{:keys [biff/job] :as ctx}]
   (prn :echo job)
@@ -35,6 +20,5 @@
 (def module
   {:tasks [{:task #'print-usage
             :schedule #(every-n-minutes 5)}]
-   :on-tx alert-new-user
    :queues [{:id :echo
              :consumer #'echo-consumer}]})
